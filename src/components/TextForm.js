01b1c146c8c3e2aa894d2f87f2de23342c265b1d
Akshata-07 <akshatashinde8552@gmail.com>
2022-08-12 18:53:36 +0530

    trial commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,21 +1,19 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-  
-  const handleUpClick = () =>{
+  const handleUpClick = () => {
     // console.log("Uppercase was clicked" + Text );
     let newText = Text.toUpperCase();
-    setText(newText)
+    setText(newText);
     props.showAlert("converted to uppercase!", "success");
-  }
+  };
 
-  const handleLoClick = () =>{
+  const handleLoClick = () => {
     // console.log("Uppercase was clicked" + Text );
     let newText = Text.toLowerCase();
-    setText(newText)
+    setText(newText);
     props.showAlert("converted to Lowercase!", "success");
-   }
+  };
 
   const handleReverse = (event) => {
     /* Convert string to array*/
@@ -26,43 +24,67 @@ export default function TextForm(props) {
     let newText = strArr.join("");
     setText(newText);
     props.showAlert("your text has been reversed!", "success");
-  }
+  };
 
-  const handleClearText = () =>{
+  const handleClearText = () => {
     let newText = " ";
-    setText(newText)
+    setText(newText);
     props.showAlert("cleared Text!", "success");
-  }
+  };
 
-
-  const handleOnChange = (event) =>{
-      // console.log("On change" );
-      setText(event.target.value)
-  }   
+  const handleOnChange = (event) => {
+    // console.log("On change" );
+    setText(event.target.value);
+  };
 
   const [Text, setText] = useState("");
   return (
     <>
-  <div className="container" style={{color: props.mode === "dark"? "white":"#042734"}}>
-  
-  <h1>{props.heading}</h1>
-  <div className="mb-3">
-  <textarea className="form-control" value={Text} onChange={handleOnChange} style={{backgroundColor: props.mode === "dark"? "grey":"white",color: props.mode === "dark"? "white":"#042734"}} id="myBox" rows="8"></textarea>
-  </div>
-  <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>convert to Uppercase</button>
-  <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>convert to Lowercase</button>
-  <button className="btn btn-primary mx-1 my-1" onClick={handleReverse}>Reverse Text</button>
-  <button className="btn btn-primary mx-1 my-1" onClick={handleClearText}>Clear Text</button>
-  </div>
-   <div className="conatiner my-3" style={{color: props.mode === "dark"? "white":"#042734"}}>
-     <h2>your text summary</h2>
-    <p>{Text.length>0 ? Text.trim().split(" ").length : 0}</p>
-    <p>{0.008 * Text.split(" ").length} Minutes Read</p>
-    <h2>Preview</h2>
-    <p>{Text.length>0?Text:"Enter something in the text box above to preview it here"}</p>
-  </div>
-  </>
-  )  
-  }
-
-
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042734" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={Text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042734",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>
+          convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>
+          convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleReverse}>
+          Reverse Text
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleClearText}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="conatiner my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042734" }}
+      >
+        <h2>your text summary.</h2>
+        <p>{Text.length > 0 ? Text.trim().split(" ").length : 0}</p>
+        <p>{0.008 * Text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>
+          {Text.length > 0
+            ? Text
+            : "Enter something in the text box above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
